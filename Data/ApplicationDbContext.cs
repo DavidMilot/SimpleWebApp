f131bb0b74dbf1695431dc6137ac1261cd94fbd8
DavidMilot <milot_david@hotmail.com>
2021-06-04 13:35:49 -0400

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SimpleWebApp.Models;
+
+namespace SimpleWebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<SimpleWebApp.Models.Joke> Joke { get; set; }
+    }
+}
